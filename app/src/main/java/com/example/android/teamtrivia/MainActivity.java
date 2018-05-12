537cfa9ef154f09ff00716cbd2e1093038e141fc
Roscoe Phillips <sorceo@gmail.com>
2018-05-12 09:21:54 -0400

    Added some updates as recommended.

--- app/src/main/java/com/example/android/teamtrivia/MainActivity.java
+++ app/src/main/java/com/example/android/teamtrivia/MainActivity.java
@@ -7,13 +7,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import junit.framework.Test;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Global variables
+    //Member variables
 
-    EditText nameOfTeamA, nameOfTeamB;
+    EditText nameOfTeamA;
+    EditText nameOfTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,99 +22,93 @@ public class MainActivity extends AppCompatActivity {
 
         nameOfTeamA = (EditText) findViewById(R.id.team_a_name);
         nameOfTeamB = (EditText) findViewById(R.id.team_b_name);
+
     }
 
     /**
-     * This declares/initializes team A's default score value.
+     * This declares/initializes the default score value for the respective teams.
      */
-
     int teamAScore = 0;
+
+    int teamBScore = 0;
 
     /**
      * This will give Team A 10 points for an easy question.
      */
-
     public void teamA10Pts(View view) {
-        teamAScore = teamAScore + 10;
+        teamAScore += 10;
         displayForTeamA(teamAScore);
     }
+
     /**
      * This will give Team A 30 points for a moderately challenging question.
      */
-
     public void teamA30Pts(View view) {
-        teamAScore = teamAScore + 30;
+        teamAScore += 30;
         displayForTeamA(teamAScore);
     }
+
     /**
      * This will give Team A 50 points for a hard question.
      */
-
     public void teamA50Pts(View view) {
-        teamAScore = teamAScore + 50;
+        teamAScore += 50;
         displayForTeamA(teamAScore);
     }
+
     /**
      * Team A will be penalized 10 points.
      */
 
     public void teamAMiss(View view) {
-        teamAScore = teamAScore - 10;
+        teamAScore -= 10;
         displayForTeamA(teamAScore);
     }
 
     /**
      * Displays the given score for Team A.
      */
-
     private void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * This declares/initializes team B's default score value.
-     */
-
-    int teamBScore = 0;
 
     /**
      * This will give Team B 10 points for an easy question.
      */
-
     public void teamB10Pts(View view) {
-        teamBScore = teamBScore + 10;
+        teamBScore += 10;
         displayForTeamB(teamBScore);
     }
+
     /**
      * This will give Team B 30 points for a moderately challenging question.
      */
-
     public void teamB30Pts(View view) {
-        teamBScore = teamBScore + 30;
+        teamBScore += 30;
         displayForTeamB(teamBScore);
     }
+
     /**
      * This will give Team B 50 points for a hard question.
      */
-
     public void teamB50Pts(View view) {
-        teamBScore = teamBScore + 50;
+        teamBScore += 50;
         displayForTeamB(teamBScore);
     }
+
     /**
      * Team B will be penalized 10 points.
      */
-
     public void teamBMiss(View view) {
-        teamBScore = teamBScore - 10;
+        teamBScore -= 10;
         displayForTeamB(teamBScore);
     }
 
     /**
      * Displays the given score for Team B.
      */
-
     private void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
@@ -123,15 +117,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This will reset the scores for both teams.
      */
-
-    public void resetTeamScores (View view) {
+    public void resetTeamScores(View view) {
         teamAScore = 0;
         displayForTeamA(teamAScore);
         teamBScore = 0;
         displayForTeamB(teamBScore);
     }
 
-    public void submitScores (View v) {
+    /**
+     * If clicked, this will provide a Toast message of who won the trivia game.
+     */
+    public void submitScores(View view) {
 
         String nameA = nameOfTeamA.getText().toString();
         String nameB = nameOfTeamB.getText().toString();
@@ -158,10 +154,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
-
-
-
-
 
 }
